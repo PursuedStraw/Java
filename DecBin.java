e4159f16f90073731dd6c7e57291a76a39eb629c
Shawn C. Dingus <Pursuedstraw@Outlook.com>
2016-09-30 11:53:49 -0700

    Add files via upload

---
+++ DecBin.java
@@ -0,0 +1,26 @@
+class DecBin{
+		public static void main (String a[]{
+		
+		int n;
+		string bin; string hex;
+		for (n = 0; n <= 255; n+){
+						bin = Integer.toBinarystring(n);
+						hex = Integer.toHexString(n);
+						System.out.println(n+" "+hex+" "+bin);
+						// concatination
+						//java DecBin > DecinOut.txt
+						
+				}
+				
+		}
+
+ }
+ 
+/*
+ * 
+ *  System.out.println(bin);
+  							System.out.print(" ");
+  							System.out.print(hex);
+  							System.out.println();
+ * 
+ * /
